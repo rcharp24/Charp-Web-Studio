eef829fad9041f2455fefbbc832f0149a57a655e
rcharp24 <robj.charpentier@gmail.com>
2026-02-23 11:38:07 -0600

    Delete generated content for home page and css with my own code

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function onAboutClick() {
+    window.location.href="../pages/AboutPage.html"
+}
+function onHomeClick() {
+    window.location.href="../pages/HomePage.html"
+}
+function onServicesClick() {
+    window.location.href="../pages/ServicesPage.html"
+}
+function onProjectsClick() {
+    window.location.href="../pages/ProjectsPage.html"
+}
