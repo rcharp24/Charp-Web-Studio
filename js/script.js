f383cab4716ee6ba836a012d324f20f5fbc6a24b
rcharp24 <160549758+rcharp24@users.noreply.github.com>
2026-03-12 21:32:44 +0000

    updates to UI

--- js/script.js
+++ js/script.js
@@ -15,3 +15,28 @@ function onQuoteClick() {
     // potential path issues when the site is served or opened via file://
     window.location.href="ContactPage.html";
 }
+const lenis = new Lenis({
+    duration: 1.2,
+    smooth: true
+});
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+const sections = document.querySelectorAll(".fade-section");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("visible");
+    }
+  });
+},{
+  threshold:0.2
+});
+
+sections.forEach(section=>{
+  observer.observe(section);
+});
